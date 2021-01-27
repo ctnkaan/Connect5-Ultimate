0385f718e10a608f603cbbda3d469c60407af032
Çetin Kaan Taşkıngenç <kaan.taskingenc@gmail.com>
2021-01-27 04:45:12 +0300

    added basic online drawing by clicking

--- client/src/client.js
+++ client/src/client.js
@@ -5,9 +5,9 @@ const log = (text) => {
   
     parent.appendChild(el);
     parent.scrollTop = parent.scrollHeight;
-  };
+};
   
-  const onChatSubmitted = (sock) => (e) => {
+const onChatSubmitted = (sock) => (e) => {
     e.preventDefault();
   
     const input = document.querySelector('#chat');
@@ -15,14 +15,47 @@ const log = (text) => {
     input.value = '';
 
     sock.emit("message", text);
-  };
-  
-  (() => {
-    const sock = io();
+};
 
-    sock.on("message", log);
+const getBoard = (canvas) => {
+
+  const con = canvas.getContext("2d");
+
+  const fillRect = (x,y,color) => {
+    con.fillStyle = color;
+    con.fillRect(x-10,y-10,20,20);
+  };
+
+  return {fillRect};
+};
+
+const getClickCords = (element, ev) => {
+  const {top, left} = element.getBoundingClientRect();
+  const {clientX, clientY} = ev;
   
-    document
-      .querySelector('#chat-form')
-      .addEventListener('submit', onChatSubmitted(sock));
-  })();
+  return {
+    x: clientX - left,
+    y: clientY - top
+  };
+};
+  
+(() => {
+
+  const canvas = document.querySelector("canvas");
+  const { fillRect } = getBoard(canvas);
+  const sock = io();
+
+  const onClick = (e) => {
+    const {x,y} = getClickCords(canvas, e);
+    sock.emit("turn", {x,y});
+  };
+
+  sock.on("message", log);
+  sock.on("turn", ({x,y}) => fillRect(x,y));
+  
+  document
+    .querySelector('#chat-form')
+    .addEventListener('submit', onChatSubmitted(sock));
+  document
+    .addEventListener("click", onClick);
+})();
--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ io.on("connection", (sock) => {
     sock.emit("message", "----- Connected -----");
 
     sock.on("message", (text) => io.emit("message", text));
+    sock.on("turn", ({x,y}) => io.emit("turn", {x,y}));
 });
 
 server.listen(3000, () => {
