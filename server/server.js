49424bc713d5a12573fc8382abd6159d07161731
Çetin Kaan Taşkıngenç <kaan.taskingenc@gmail.com>
2021-01-30 06:02:22 +0300

    added turns

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const socketio = require("socket.io");
 const randomColor = require("randomcolor");
 const createBoard = require("./create-board");
+const createCooldown = require("./createCooldown");
 
 const app = express();
 
@@ -13,17 +14,32 @@ const server = http.createServer(app);
 const io = socketio(server);
 const {clear,getBoard,makeTurn} = createBoard(20);
 
-io.on("connection", (sock) => {
+io.on('connection', (sock) => {
+    io.emit("message","----PLAYER CONNCECTED----")
     const color = randomColor();
-    sock.emit("board", getBoard());
-    sock.emit("message", "----- Connected -----");
 
-    sock.on("message", (text) => io.emit("message", text));
-    sock.on("turn", ({x,y}) => {
-        makeTurn(x,y,color);
-        io.emit("turn", {x,y,color})
-    });
-});
+    const cooldown = createCooldown(2000);
+
+    sock.on('message', (text) => io.emit('message', text));
+  
+    const onTurn = ({ x, y }) => {
+      if (cooldown()) {
+        io.emit('turn', { x, y, color });
+        const playerWin = makeTurn(x, y, color);
+  
+        if (playerWin) {
+          sock.emit('message', '----YOU WIN----');
+          io.emit('message', '----NEW ROUND----');
+          clear();
+          io.emit('board');
+        }
+      }
+    };
+  
+    sock.on('turn', onTurn);
+  
+    sock.emit('board', getBoard());
+  });
 
 server.listen(3000, () => {
     console.log("server started port 3000");
